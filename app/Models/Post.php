6d9643dd211cfcfac7f56d71ff5087a8097a83d0
Diana <diana.p.m.q@gmail.com>
2024-05-15 11:42:01 -0600

    add softdeletes migration

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,12 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Permission\Traits\HasRoles;
 
 
 class Post extends Model
 {
-    use HasFactory, HasRoles;
+    use HasFactory, HasRoles, SoftDeletes;
 
     public function user() : BelongsTo
     {
